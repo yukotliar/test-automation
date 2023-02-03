9c3a9ea0d9510d2532f3dad0f1fb75436f77b36f
yurii.kotliar <yukotliar@gmail.com>
2023-02-03 15:24:59 +0200

    login page tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators:
+    login_form = (By.CSS_SELECTOR, "#login_form")
+    login_email = (By.CSS_SELECTOR, "#id_login-username")
+    login_password = (By.CSS_SELECTOR, "#id_login-password")
+    login_button = (By.CSS_SELECTOR, "[name='login_submit']")
+
+    register_form = (By.CSS_SELECTOR, "#register_form")
+    register_email = (By.CSS_SELECTOR, "#id_registration-email")
+    register_password_1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    register_password_2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    register_button = (By.CSS_SELECTOR, "[name='registration_submit']")
---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "That's not a correct URL for login page"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.login_form), "Login form is not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.register_form), "Register form is not present"
